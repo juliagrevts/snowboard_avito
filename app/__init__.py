22bda1237074ff7963c366862f98152e3ef114c5
Julia Grevtseva <j.grevts@gmail.com>
2022-07-30 19:02:59 +0300

    Users registration and login * to control the login process LoginManager class created and app object configured for login; * registration, login and logout view functions created; * RegistrationForm and LoginForm created; * Blueprint ‘user’ created and registered with the app; * model ‘User’ declared, model 'Snowboard' updated to relate it to 'User' model; * due to the inability to change the Sqlite database migrations mechanism reinitialized and database recreated; * login and registration html templates created; * requirements updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 
 from app.db import db
 from app.product.views import blueprint as product_blueprint
+from app.user.views import blueprint as user_blueprint
+from app.user.models import User
 
 
 def create_app(): 
@@ -11,6 +14,15 @@ def create_app():
     db.init_app(app)
     migrate = Migrate(app, db)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'user.login'
+
     app.register_blueprint(product_blueprint)
+    app.register_blueprint(user_blueprint)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
     return app
