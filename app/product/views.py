0500c17b7c52c35b8fa479b7a2b0487ba90fc583
Julia Grevtseva <j.grevts@gmail.com>
2022-08-07 18:12:52 +0300

    Feature/main page (#5)
    * Requirements.txt, functions get_snowboard_page_html, that parse and return the ad data from snowboard page on avito, and parse_placement_date, that brings the dates to a single format and converts them from a string to datetime
    
    * Flask app and the initial version of the database creation
    
    * Flask app creation
    * Sqlalchemy object created and model 'Snowboard' declared
    * get_snowboard_page_html refactored
    * requirements and gitignore changed
    
    * Rewriting the parser with Selenium, saving data to the database
    * in order to avoid being blocked by Avito the parser is rewritten with Selenium
    * save_product, save_snowboard and save_photo_links functions, that save data parsed from Avito to the database
    * data on snowboards parsed from Avito and saved to sqlite database
    * Blueprint ‘product’ created and registered with the app
    * templates folder and base html templates created
    * migrations added to the app, model 'Snowboard' changed, model ‘SnowboardPhotoLink’ declared
    * requirements and gitignore changed
    
    * Main page of SnowboardShop website
    * main page of the site, which displays 15 ads in a list;
    * pagination added:
    * pages with a detailed description of the ad and the ability to navigate to these pages from the list of ads added

--- app/product/views.py
+++ app/product/views.py
@@ -1,10 +1,31 @@
-from flask import Blueprint, render_template
+from flask import abort, Blueprint, render_template
+
+from app.product.models import Snowboard
 
 
 blueprint = Blueprint('product', __name__)
 
 
-@blueprint.route('/')
-def index():
-    title = 'Snowboard Shop'
-    return render_template('product/index.html', page_title=title)
+@blueprint.route('/', defaults={'page': 1})
+@blueprint.route('/<int:page>')
+def index(page):
+    title = 'КАТАЛОГ СНОУБОРДОВ'
+    per_page = 15
+    snowboards_pages_pagination = Snowboard.query.order_by(
+        Snowboard.ad_placement_date.desc()
+    ).paginate(page, per_page, False)
+    return render_template(
+        'product/index.html',
+        page_title=title,
+        snowboards_pages_pagination=snowboards_pages_pagination
+    )
+
+
+@blueprint.route('/snowboard/<int:snowboard_id>')
+def ad_page(snowboard_id):
+    snowboard = Snowboard.query.get(snowboard_id)
+    if not snowboard:
+        abort(404)
+    return render_template(
+        'product/ad_page.html', page_title=snowboard.product_name, snowboard=snowboard
+    )
